ebf41fc19c19715a45ba65c2674e0fa931c24e2c
MikeyFJr <zaphero9696@gmail.com>
2024-10-15 11:22:58 -0400

    Updated (incomplete) random movie function, started view movie details function.

--- backend.py
+++ backend.py
@@ -1,16 +1,25 @@
 #pip install omdb in terminal
 import omdb
-from omdb import OMDBClient 
+omdb.set_default('apikey', '96ae5860')
+from omdb import OMDBClient
 client = OMDBClient(apikey='96ae5860')
-import string
-string.letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 import random
-randomid=("tt" +str(random.randint(1,9000000))) #Grab random IMDb ID
-random.choice(string.ascii_letters) #Grab random first letter for searching (Currently doesn't work, exact matches of words included in a title are required)
 
-def random_movie():
-    roll = client.get(search="The Matrix") #Searches the given string in the API's movie catalog. Currently only searches exact matches.
-    print(roll)
+def random_movieid(): #OMDb does not have a random movie method, so the ID might result in a timeout
+    idnum = random.randint(1000000,9999999) #Generate random number
+    randomid=("tt" +str(idnum)) #Create random IMDb ID
+    roll = omdb.imdbid(randomid, timeout=3) #Searches the randomly generated ID in the database.
     return roll
 
-random_movie()
+def view_movie(title: str): #Enter a movie title to get a dictionary of specific information of the movie
+    searchlist = omdb.search_movie(title, page=1)
+    specific = searchlist[1]
+    usedid = specific.get('imdb_id')
+    details = omdb.imdbid(usedid, timeout=3)
+    return details
+
+case6 = random_movieid()
+case9 = view_movie("The Matrix")
+
+print(case6)
+print(case9)
