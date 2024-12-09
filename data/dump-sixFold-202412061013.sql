ba5ef9b1f8bb6f2d92fe18e20ae92f698b7c283e
Selvin <selvincastillo856@gmail.com>
2024-12-09 11:00:17 -0500

    adding to sql script

--- data/dump-sixFold-202412061013.sql
+++ data/dump-sixFold-202412061013.sql
@@ -3,25 +3,37 @@
 -- Host: localhost    Database: sixFold
 -- ------------------------------------------------------
 -- Server version	9.0.1
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
+;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+;
+/*!50503 SET NAMES utf8mb4 */
+;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */
+;
+/*!40103 SET TIME_ZONE='+00:00' */
+;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */
+;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */
+;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */
+;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */
+;
 --
 -- Table structure for table `FavoriteMovies`
 --
 
+CREATE DATABASE IF NOT EXISTS `sixFold`;
+USE `sixFold`;
 DROP TABLE IF EXISTS `FavoriteMovies`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
 CREATE TABLE `FavoriteMovies` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
@@ -30,25 +42,28 @@ CREATE TABLE `FavoriteMovies` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `favoritemovies_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=35 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 35 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
 --
 -- Dumping data for table `FavoriteMovies`
 --
 
 LOCK TABLES `FavoriteMovies` WRITE;
-/*!40000 ALTER TABLE `FavoriteMovies` DISABLE KEYS */;
-/*!40000 ALTER TABLE `FavoriteMovies` ENABLE KEYS */;
+/*!40000 ALTER TABLE `FavoriteMovies` DISABLE KEYS */
+;
+/*!40000 ALTER TABLE `FavoriteMovies` ENABLE KEYS */
+;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `MovieNotes`
 --
 
 DROP TABLE IF EXISTS `MovieNotes`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
 CREATE TABLE `MovieNotes` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
@@ -58,26 +73,43 @@ CREATE TABLE `MovieNotes` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `movienotes_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 3 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
 --
 -- Dumping data for table `MovieNotes`
 --
 
 LOCK TABLES `MovieNotes` WRITE;
-/*!40000 ALTER TABLE `MovieNotes` DISABLE KEYS */;
-INSERT INTO `MovieNotes` VALUES (1,1,'Home Alone','HELLO WORLD','2024-12-05 21:29:12'),(2,1,'Deadpool','very funny movie!','2024-12-05 22:05:46');
-/*!40000 ALTER TABLE `MovieNotes` ENABLE KEYS */;
+/*!40000 ALTER TABLE `MovieNotes` DISABLE KEYS */
+;
+INSERT INTO `MovieNotes`
+VALUES (
+    1,
+    1,
+    'Home Alone',
+    'HELLO WORLD',
+    '2024-12-05 21:29:12'
+  ),
+(
+    2,
+    1,
+    'Deadpool',
+    'very funny movie!',
+    '2024-12-05 22:05:46'
+  );
+/*!40000 ALTER TABLE `MovieNotes` ENABLE KEYS */
+;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `SearchHistory`
 --
 
 DROP TABLE IF EXISTS `SearchHistory`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
 CREATE TABLE `SearchHistory` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
@@ -86,26 +118,31 @@ CREATE TABLE `SearchHistory` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `searchhistory_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=80 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 80 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
 --
 -- Dumping data for table `SearchHistory`
 --
 
 LOCK TABLES `SearchHistory` WRITE;
-/*!40000 ALTER TABLE `SearchHistory` DISABLE KEYS */;
-INSERT INTO `SearchHistory` VALUES (78,1,'home alone','2024-12-05 19:47:42'),(79,1,'deadpool','2024-12-05 22:05:35');
-/*!40000 ALTER TABLE `SearchHistory` ENABLE KEYS */;
+/*!40000 ALTER TABLE `SearchHistory` DISABLE KEYS */
+;
+INSERT INTO `SearchHistory`
+VALUES (78, 1, 'home alone', '2024-12-05 19:47:42'),
+(79, 1, 'deadpool', '2024-12-05 22:05:35');
+/*!40000 ALTER TABLE `SearchHistory` ENABLE KEYS */
+;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `users`
 --
 
 DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
 CREATE TABLE `users` (
   `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(50) NOT NULL,
@@ -113,26 +150,43 @@ CREATE TABLE `users` (
   `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 10 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
 --
 -- Dumping data for table `users`
 --
 
 LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'testuser','testpassword','2024-10-23 16:31:06'),(5,'testuser2','testpassword2','2024-10-28 03:45:20'),(8,'testuser3','sixfold','2024-11-08 15:23:27'),(9,'testuser4','12345678','2024-11-13 15:17:59');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */
+;
+INSERT INTO `users`
+VALUES (
+    1,
+    'testuser',
+    'testpassword',
+    '2024-10-23 16:31:06'
+  ),
+(
+    5,
+    'testuser2',
+    'testpassword2',
+    '2024-10-28 03:45:20'
+  ),
+(8, 'testuser3', 'sixfold', '2024-11-08 15:23:27'),
+(9, 'testuser4', '12345678', '2024-11-13 15:17:59');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */
+;
 UNLOCK TABLES;
-
 --
 -- Table structure for table `Watchlist`
 --
 
 DROP TABLE IF EXISTS `Watchlist`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
+/*!40101 SET @saved_cs_client     = @@character_set_client */
+;
+/*!50503 SET character_set_client = utf8mb4 */
+;
 CREATE TABLE `Watchlist` (
   `id` int NOT NULL AUTO_INCREMENT,
   `user_id` int NOT NULL,
@@ -142,29 +196,35 @@ CREATE TABLE `Watchlist` (
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `watchlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
+) ENGINE = InnoDB AUTO_INCREMENT = 28 DEFAULT CHARSET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */
+;
 --
 -- Dumping data for table `Watchlist`
 --
 
 LOCK TABLES `Watchlist` WRITE;
-/*!40000 ALTER TABLE `Watchlist` DISABLE KEYS */;
-/*!40000 ALTER TABLE `Watchlist` ENABLE KEYS */;
+/*!40000 ALTER TABLE `Watchlist` DISABLE KEYS */
+;
+/*!40000 ALTER TABLE `Watchlist` ENABLE KEYS */
+;
 UNLOCK TABLES;
-
 --
 -- Dumping routines for database 'sixFold'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */
+;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */
+;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */
+;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */
+;
 -- Dump completed on 2024-12-06 10:13:49
